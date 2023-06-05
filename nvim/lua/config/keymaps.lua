d1f9424c3547065ee1bc3877709fe05854d1f359
Karim Ould Mahieddine <karimouldmahieddine@MBP-von-Karim-2.fritz.box>
2023-06-05 09:28:46 +0200

    feat(keymap): added new kemap to toggle relative line numbers

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
 local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
@@ -100,3 +113,6 @@ keymap("n", "<C-u>", "<C-u>zz", opts)
 
 -- Search
 keymap("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", opts)
+
+-- UI
+map("n", "<leader>uR", ":set relativenumber!<CR>", { desc = "Toggle relative line numbers" })
